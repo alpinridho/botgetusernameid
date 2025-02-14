5a5e03bbbea1aaee06df3d517516826d5ad93d55
alpin ridho <alpinridho2511@gmail.com>
2025-02-14 14:03:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,25 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ConversationHandler, CallbackQueryHandler
-import asyncio
-import re
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ConversationHandler, filters
+from config import TOKEN
+from handlers.admin import add_admin, list_admins, del_admin, handle_admin_deletion, cancel_list_admins, DELADM_SELECT_ADMIN
+from handlers.user import start, get_user_info
 
-# Ganti dengan token bot kamu
-TOKEN = "7870248619:AAEVVd85yasA4lYKNpJAPLO4vrsEVL9Pvyg"
-
-async def start(update: Update, context) -> None:
-    user = update.message.from_user
-    await update.message.reply_text(f"Halo {user.first_name}! Kirimkan pesan apa saja untuk mendapatkan ID dan username kamu.")
-
-async def get_user_info(update: Update, context) -> None:
-    user = update.message.from_user
-    user_info = f"🆔 ID: {user.id}\n👤 Username: @{user.username if user.username else 'Tidak ada'}"
-    await update.message.reply_text(user_info)
-
-# =================================== MAIN =============================================================
 def main():
     application = ApplicationBuilder().token(TOKEN).build()
 
+    del_admin_handler = ConversationHandler(
+        entry_points=[CommandHandler('deladm', del_admin)],
+        states={DELADM_SELECT_ADMIN: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_admin_deletion)]},
+        fallbacks=[CommandHandler("cancel", cancel_list_admins)]
+    )
+
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, get_user_info))
-    
+    application.add_handler(CommandHandler("addadmin", add_admin))
+    application.add_handler(CommandHandler("listadm", list_admins))
+    application.add_handler(del_admin_handler)
+
     print("Bot berjalan...")
     application.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
